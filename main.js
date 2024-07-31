e3609549f0e7f8450a4c0d002620266384c82204
João Gabriel Ramos <jgbvramos@id.uff.br>
2024-07-31 01:24:52 -0300

    feat: criando função para postar msg

---
+++ main.js
@@ -0,0 +1,13 @@
+function enviar_msg() {
+    let input = document.querySelector(".caixaMsg");
+    let card = document.createElement("div");
+    let texto = document.createElement("p");
+    texto.innerText = input.value;
+    card.append(texto);
+
+    let secao = document.querySelector(".espaco");
+    secao.append(card);
+}   
+
+let btn_enviar = document.querySelector(".btn_enviar");
+btn_enviar.addEventListener("click", ()=>{enviar_msg()})
