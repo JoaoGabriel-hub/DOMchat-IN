4e954620200920d525ac98a8b10e1262e6fb2953
João Gabriel Ramos <jgbvramos@id.uff.br>
2024-07-31 14:34:25 -0300

    feat: criando div com os botões junto ao card de msg

--- main.js
+++ main.js
@@ -4,10 +4,25 @@ function enviar_msg() {
     let texto = document.createElement("p");
     texto.innerText = input.value;
     card.append(texto);
-    card.classList.add("card")
+    card.classList.add("card");
+
+    let botaoContainer = document.createElement("div");
+    botaoContainer.classList.add("botao-container");
+
+    let btn_editar = document.createElement("button");
+    btn_editar.innerText = "Editar";
+    btn_editar.classList.add("btn_editar");
+    botaoContainer.append(btn_editar);
+
+    let btn_excluir = document.createElement("button");
+    btn_excluir.innerText = "Excluir";
+    btn_excluir.classList.add("btn_excluir");
+    botaoContainer.append(btn_excluir);
 
     let secao = document.querySelector(".espaco");
     secao.append(card);
+    secao.append(botaoContainer);
+    input.value = '';
 }   
 
 let btn_enviar = document.querySelector(".btn_enviar");
